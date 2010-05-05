2f62f2afba02fbef39a79176c80ad1f22f372b5f
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2010-05-05 12:41:58 +0000

    *** empty log message ***

--- bin/jet_weighted_spectrum_x.cc
+++ bin/jet_weighted_spectrum_x.cc
@@ -43,65 +43,59 @@ int main(int argc,char**argv)
   if (!cl.parse(argc,argv)) return 0;
   
   vector<string> inputs   = cl.getVector<string>("inputs");
-  string         datapath = cl.getValue <string>("datapath",        "");
-  string         algorithm= cl.getValue <string>("algorithm","ak5calo");
-  bool           logx     = cl.getValue <bool>  ("logx",         false);
-  bool           logy     = cl.getValue <bool>  ("logy",          true);
-  bool           batch    = cl.getValue <bool>  ("batch",        false);
-  vector<string> formats  = cl.getVector<string>("formats",         "");
-
+  string         datapath = cl.getValue <string>("datapath",               "");
+  string         algorithm= cl.getValue <string>("algorithm",       "ak5calo");
+  string         variable = cl.getValue <string>("variable",          "refpt");
+  string         xtitle   = cl.getValue <string>("xtitle","p_{T}^{GEN} [GeV]");
+  bool           logx     = cl.getValue <bool>  ("logx",                false);
+  bool           logy     = cl.getValue <bool>  ("logy",                 true);
+  bool           batch    = cl.getValue <bool>  ("batch",               false);
+  vector<string> formats  = cl.getVector<string>("formats",                "");
+  
   if(!cl.check()) return 0;
   cl.print();
   
+  if (datapath.empty()) datapath=".";
+  
   argc = (batch) ? 2 : 1; if (batch) argv[1] = (char*)"-b";
   TApplication* app = new TApplication("jet_weighted_spectrum_x",&argc,argv);
-
+  
   //TH1::SetDefaultSumw2();
   set_root_style();
   gStyle->SetOptStat(0);
   TColor::SetPalette(1,0);
   
-  TCanvas* cRefPt  = new TCanvas("RefPt" ,"RefPt",   0,0,790,600);
-  TCanvas* cRefPtW = new TCanvas("RefPtW","RefPtW",800,0,790,600);
-
+  TCanvas* c  = new TCanvas(variable.c_str(),
+			    variable.c_str(),        0,0,790,600);
+  TCanvas* cW = new TCanvas((variable+"W").c_str(),
+			    (variable+"W").c_str(),800,0,790,600);
+  
   TLegend* leg = new TLegend(0.835,0.96,1.0,0.145);
   leg->SetLineColor(10);
   leg->SetFillColor(10);
   leg->SetBorderSize(0);
-
+  
   THStack* stRefPt  = new THStack("RefPt", "");
   THStack* stRefPtW = new THStack("RefPtW","");
-
-  for (unsigned int i=0;i<inputs.size();i++) {
+  
+  for (unsigned i=0;i<inputs.size();i++) {
     size_t pos      = inputs[i].find(":");
     string sample   = inputs[i].substr(0,pos);
     string filename = sample + ".root";
-
     float  weight   = 1.0;
-    if (pos!=string::npos) {
-      stringstream ss;ss<<inputs[i].substr(pos+1); ss>>weight;
-    }
-
+    if (pos!=string::npos){stringstream ss;ss<<inputs[i].substr(pos+1);ss>>weight;}
+    
     // DEBUG
     cout<<"filename="<<filename<<", weight="<<weight<<endl;
     
     TFile* file = new TFile((datapath+"/"+filename).c_str(),"READ");
-    if (!file->IsOpen()) {
-      cout<<"Can't open file "<<filename<<endl;
-      continue;
-    }
-
+    if (!file->IsOpen()) { cout<<"Can't open file "<<filename<<endl; continue; }
+    
     TDirectory* dir = (TDirectory*)file->Get(algorithm.c_str());
-    if (0==dir) {
-      cout<<"file "<<filename<<" does not contain dir "<<algorithm<<endl;
-      continue;
-    }
-
+    if (0==dir) { cout<<"No dir "<<algorithm<<"found in "<<filename<<endl;continue;}
+    
     TTree* tree = (TTree*)dir->Get("t");
-    if (0==tree) {
-      cout<<"dir "<<algorithm<<" does not contain tree t"<<endl;
-      continue;
-    }
+    if (0==tree) {cout<<"No tree 't' in dir "<<algorithm<<endl; continue; }
     
     TH1F* hRefPt  = new TH1F("RefPt", "",1000,0,4500);
     TH1F* hRefPtW = new TH1F("RefPtW","",1000,0,4500);
@@ -109,8 +103,8 @@ int main(int argc,char**argv)
     weight /= tree->GetEntries();
     stringstream wsel; wsel<<weight<<"*(1)";
 
-    tree->Project("RefPt", "refpt");
-    tree->Project("RefPtW","refpt",wsel.str().c_str());
+    tree->Project("RefPt", variable.c_str());
+    tree->Project("RefPtW",variable.c_str(),wsel.str().c_str());
     
     Color_t color = TColor::GetColorPalette(i);
 
@@ -130,28 +124,28 @@ int main(int argc,char**argv)
     stRefPtW->Add(hRefPtW);
   }
 
-  cRefPt->cd();
+  c->cd();
   gPad->SetLeftMargin(0.1);
   gPad->SetRightMargin(0.17);
   stRefPt->Draw();
-  stRefPt->GetXaxis()->SetTitle("p_{T}^{GEN} [GeV]");
+  stRefPt->GetXaxis()->SetTitle(xtitle.c_str());
   stRefPt->SetMinimum(9.E-01);
   if (logx) gPad->SetLogx();
   if (logy) gPad->SetLogy();
   leg->Clone()->Draw();
-  cRefPt->Update();
+  c->Update();
 
-  cRefPtW->cd();
+  cW->cd();
   gPad->SetLeftMargin(0.1);
   gPad->SetRightMargin(0.17);
   stRefPtW->Draw();
-  stRefPtW->GetXaxis()->SetTitle("p_{T}^{GEN} [GeV]");
+  stRefPtW->GetXaxis()->SetTitle(xtitle.c_str());
   stRefPtW->SetMinimum(9E-11);
   stRefPtW->SetMaximum(5.E09);
   if (logx) gPad->SetLogx();
   if (logy) gPad->SetLogy();
   leg->Clone()->Draw();
-  cRefPtW->Update();
+  cW->Update();
 
   if (!batch) app->Run();
   
