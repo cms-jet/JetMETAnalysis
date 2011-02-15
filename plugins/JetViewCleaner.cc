4988ee131cafe1f0425cca0e84dc1f859be44abe
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2011-02-15 08:49:21 +0000

    tag V00-08-08: see ChangeLog.

--- plugins/JetViewCleaner.cc
+++ plugins/JetViewCleaner.cc
@@ -12,11 +12,11 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
- 
+
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
- 
+
 #include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/JetReco/interface/JetCollection.h"
 #include "DataFormats/Candidate/interface/CandidateFwd.h"
@@ -37,7 +37,7 @@ public:
   // construction/destruction
   JetViewCleaner(const edm::ParameterSet& iConfig);
   virtual ~JetViewCleaner();
-  
+
   // member functions
   void produce(edm::Event& iEvent,const edm::EventSetup& iSetup);
   void endJob();
@@ -47,7 +47,8 @@ private:
   edm::InputTag              srcJets_;
   std::vector<edm::InputTag> srcObjects_;
   double                     deltaRMin_;
-  
+  double                     deltaRMax_;
+
   std::string  moduleLabel_;
   unsigned int nJetsTot_;
   unsigned int nJetsClean_;
@@ -66,6 +67,7 @@ JetViewCleaner::JetViewCleaner(const edm::ParameterSet& iConfig)
   : srcJets_    (iConfig.getParameter<edm::InputTag>         ("srcJets"))
   , srcObjects_ (iConfig.getParameter<vector<edm::InputTag> >("srcObjects"))
   , deltaRMin_  (iConfig.getParameter<double>                ("deltaRMin"))
+  , deltaRMax_  (iConfig.getParameter<double>                ("deltaRMax"))
   , moduleLabel_(iConfig.getParameter<string>                ("@module_label"))
   , nJetsTot_(0)
   , nJetsClean_(0)
@@ -94,14 +96,23 @@ void JetViewCleaner::produce(edm::Event& iEvent,const edm::EventSetup& iSetup)
 
   edm::Handle<reco::JetView> jets;
   iEvent.getByLabel(srcJets_,jets);
-  
+
   bool* isClean = new bool[jets->size()];
-  for (unsigned int iJet=0;iJet<jets->size();iJet++) isClean[iJet] = true;
-  
+
   for (unsigned int iSrc=0;iSrc<srcObjects_.size();iSrc++) {
     edm::Handle<reco::CandidateView> objects;
     iEvent.getByLabel(srcObjects_[iSrc],objects);
-    
+
+    for (unsigned int iJet=0;iJet<jets->size();iJet++) {
+      const reco::Jet& jet = jets->at(iJet);
+      isClean[iJet] = false;
+      for (unsigned int iObj=0;iObj<objects->size();iObj++) {
+	const reco::Candidate& obj = objects->at(iObj);
+	double deltaR = reco::deltaR(jet,obj);
+	if (deltaR<deltaRMax_) isClean[iJet] = true;
+      }
+    }
+
     for (unsigned int iJet=0;iJet<jets->size();iJet++) {
       const reco::Jet& jet = jets->at(iJet);
       for (unsigned int iObj=0;iObj<objects->size();iObj++) {
@@ -111,14 +122,14 @@ void JetViewCleaner::produce(edm::Event& iEvent,const edm::EventSetup& iSetup)
       }
     }
   }
-  
+
   for (unsigned int iJet=0;iJet<jets->size();iJet++)
     if (isClean[iJet]) cleanJets->push_back(jets->refAt(iJet));
-  
+
   nJetsTot_  +=jets->size();
   nJetsClean_+=cleanJets->size();
 
-  delete [] isClean;  
+  delete [] isClean;
   iEvent.put(cleanJets);
 }
 
