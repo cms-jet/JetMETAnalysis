412e43dff98d029674ca774317245c6a227c56dc
Alexx Perloff <aperloff@physics.tamu.edu>
2013-12-28 16:39:14 -0600

    First commit of the files used for creating the custom JRAEvent class in an EDProducer.

---
+++ src/classes.h
@@ -0,0 +1,12 @@
+#include "JetMETAnalysis/JetUtilities/interface/JRAEvent.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <vector>
+
+namespace { 
+   struct dictionary {
+      JRAEvent jraeventdummy0;
+      edm::Wrapper<JRAEvent> dummy1;
+      std::vector<JRAEvent> dummy2;
+      edm::Wrapper<std::vector<JRAEvent> > dummy3;
+   };
+}
