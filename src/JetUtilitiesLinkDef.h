1cf9221ce2844dfc6b1e38f6751b09391655945c
Alexx Perloff <aperloff@physics.tamu.edu>
2014-01-19 16:26:51 -0600

    Rename TProfileMDLinkDef.h to JetUtilitiesLinkDef.h

--- src/JetUtilitiesLinkDef.h
+++ src/JetUtilitiesLinkDef.h
@@ -1,5 +1,6 @@
 #include "JetMETAnalysis/JetUtilities/interface/TProfileMDD.h"
 #include "JetMETAnalysis/JetUtilities/interface/TProfileMDF.h"
+#include "JetMETAnalysis/JetUtilities/interface/JRANtuple.h"
 
 #ifdef __CINT__
 #pragma link off all globals;
@@ -9,5 +10,6 @@
 
 #pragma link C++ class TProfileMDD+;
 #pragma link C++ class TProfileMDF+;
+#pragma link C++ class JRANtuple+;
 
 #endif
