81d93a495e96f0056bffbe3ee9a696e4c110f590
Alexx Perloff <aperloff@physics.tamu.edu>
2014-09-03 16:33:25 -0500

    Update for testing

--- test/run_jra_cfg.py
+++ test/run_jra_cfg.py
@@ -24,18 +24,19 @@ inputFiles = cms.untracked.vstring(
 #############################
 # JRAP PATTuple (Published) #
 #############################
-'/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_PFchs_53X/ea497ea51a493fe0c576c422ffa32b60/JRAP_264_1_ivm.root',
-'/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_PFchs_53X/ea497ea51a493fe0c576c422ffa32b60/JRAP_265_1_DUv.root',
-'/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_PFchs_53X/ea497ea51a493fe0c576c422ffa32b60/JRAP_266_1_LHj.root',
+#'/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_PFchs_53X/ea497ea51a493fe0c576c422ffa32b60/JRAP_264_1_ivm.root',
+#'/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_PFchs_53X/ea497ea51a493fe0c576c422ffa32b60/JRAP_265_1_DUv.root',
+#'/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_PFchs_53X/ea497ea51a493fe0c576c422ffa32b60/JRAP_266_1_LHj.root',
 ###############################
 # JRAP PATTuple (Unpublished) #
 ###############################
 #'file:/fdata/hepx/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_53X/bcedc6301a9ce8b39e91810d93214040/JRAP_287_1_0s2.root',
 #'file:/fdata/hepx/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_53X/bcedc6301a9ce8b39e91810d93214040/JRAP_288_1_AF4.root',
 #'file:/fdata/hepx/store/user/aperloff/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/PAT_JRAP_53X/bcedc6301a9ce8b39e91810d93214040/JRAP_289_1_T7V.root',
+'file:JRAP.root'
     )
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
 process.source = cms.Source("PoolSource",
                             skipEvents = cms.untracked.uint32(0),
                             fileNames = inputFiles )
@@ -43,7 +44,7 @@ process.source = cms.Source("PoolSource",
 
 algsizetype = {'ak':[4]}
 #algsizetype = {'ak':[3,4,5,6,7,8,9,10]}
-jettype = ['pf']
+jettype = ['pf','pfchs']
 #jettype = ['calo','pf','pfchs']
 corrs = ['']
 #corrs = ['','l1','l2l3']
@@ -53,9 +54,9 @@ algorithms = []
 
 for k, v in algsizetype.iteritems():
 	for s in v:
-		size.append(s/20.0)
 		for j in jettype:
 			for c in corrs:
+				size.append(s/20.0)
 				algorithms.append(str(k+str(s)+j+c))
 
 
