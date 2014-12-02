f8830a4f33b643e833eb01c27446d2f8abc8852b
Alexx Perloff <aperloff@physics.tamu.edu>
2014-12-01 21:50:31 -0600

    Update the style to mod14 from Mikko.

--- JetAnalyzers/interface/Style.h
+++ JetAnalyzers/interface/Style.h
@@ -1,23 +1,34 @@
+#ifndef STYLE_H
+#define STYLE_H
+
 #include "TStyle.h"
 #include "TROOT.h"
 #include "TPad.h"
+#include "TFrame.h"
 #include "TLatex.h"
 #include "TGraph.h"
 #include "TLegend.h"
 #include "TCanvas.h"
 #include "TH1.h"
 #include "TGraph.h"
+#include "THStack.h"
 
+#include <iostream>
 #include <string>
 #include <assert.h>
 
+using std::cout;
+using std::endl;
 using std::string;
 
 // colors to use
 EColor tdrColors[13] = {kBlack, kBlue, kRed, kGreen, kYellow, kMagenta, kCyan,
                         kOrange, kSpring, kTeal, kAzure, kViolet, kPink};
 
-// Add useful short-hands
+////////////////////////////////////
+// Useful small macros (by Mikko) //
+////////////////////////////////////
+
 void tdrDraw(TH1* h, string opt,
        int marker=kFullCircle, int mcolor = kBlack,
        int lstyle=kSolid, int lcolor=-1,
@@ -29,6 +40,19 @@ void tdrDraw(TH1* h, string opt,
   h->SetFillStyle(fstyle);
   h->SetFillColor(fcolor);
   h->Draw((opt+"SAME").c_str());
+}
+
+void tdrDraw(THStack* s, string opt,
+       int marker=kFullCircle, int mcolor = kBlack,
+       int lstyle=kSolid, int lcolor=-1,
+       int fstyle=1001, int fcolor=kYellow+1) {
+  //s->SetMarkerStyle(marker);
+  //s->SetMarkerColor(mcolor);
+  //s->SetLineStyle(lstyle);
+  //s->SetLineColor(lcolor==-1 ? mcolor : lcolor);
+  //s->SetFillStyle(fstyle);
+  //s->SetFillColor(fcolor);
+  s->Draw((opt+"SAME").c_str());
 }
 
 void tdrDraw(TGraph* g, string opt,
@@ -53,9 +77,21 @@ TLegend *tdrLeg(double x1, double y1, double x2, double y2) {
   return leg;
 }
 
+//////////////////////////////////////////
+// New CMS Style from 2014              //
+// https://ghm.web.cern.ch/ghm/plots/   //
+// Merged all macros into one
+//////////////////////////////////////////
+
+////////////////
+// tdrstyle.C //
+////////////////
+
+
 // tdrGrid: Turns the grid lines on (true) or off (false)
 
-void tdrGrid(TStyle* tdrStyle, bool gridOn) {
+void tdrGrid(bool gridOn) {
+  TStyle *tdrStyle = (TStyle*)gROOT->FindObject("tdrStyle"); assert(tdrStyle);
   tdrStyle->SetPadGridX(gridOn);
   tdrStyle->SetPadGridY(gridOn);
 }
@@ -63,11 +99,10 @@ void tdrGrid(TStyle* tdrStyle, bool gridOn) {
 // fixOverlay: Redraws the axis
 
 void fixOverlay() {
-   gPad->RedrawAxis();
+  gPad->RedrawAxis();
 }
 
-TStyle* getTDRStyle() {
-
+void setTDRStyle() {
   TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
 
 // For the canvas:
@@ -75,7 +110,7 @@ TStyle* getTDRStyle() {
   tdrStyle->SetCanvasColor(kWhite);
   tdrStyle->SetCanvasDefH(600); //Height of canvas
   tdrStyle->SetCanvasDefW(600); //Width of canvas
-  tdrStyle->SetCanvasDefX(0);   //Position on screen
+  tdrStyle->SetCanvasDefX(0);   //POsition on screen
   tdrStyle->SetCanvasDefY(0);
 
 // For the Pad:
@@ -96,7 +131,7 @@ TStyle* getTDRStyle() {
   tdrStyle->SetFrameLineColor(1);
   tdrStyle->SetFrameLineStyle(1);
   tdrStyle->SetFrameLineWidth(1);
-
+  
 // For the histo:
   // tdrStyle->SetHistFillColor(1);
   // tdrStyle->SetHistFillStyle(0);
@@ -107,15 +142,13 @@ TStyle* getTDRStyle() {
   // tdrStyle->SetNumberContours(Int_t number = 20);
 
   tdrStyle->SetEndErrorSize(2);
-  //  tdrStyle->SetErrorMarker(20);
-  tdrStyle->SetErrorX(0.);
+  // tdrStyle->SetErrorMarker(20);
+  //tdrStyle->SetErrorX(0.);
   
-  //tdrStyle->SetMarkerStyle(20);
-  //tdrStyle->SetMarkerStyle(7);
-
+  tdrStyle->SetMarkerStyle(20);
+  
 //For the fit/function:
-  tdrStyle->SetOptFit(1); // To turn of the fit statistics: SetOptFit(0);
-  // tdrStyle->SetOptFit(2211);
+  tdrStyle->SetOptFit(1);
   tdrStyle->SetFitFormat("5.4g");
   tdrStyle->SetFuncColor(2);
   tdrStyle->SetFuncStyle(1);
@@ -129,9 +162,7 @@ TStyle* getTDRStyle() {
 // For the statistics box:
   tdrStyle->SetOptFile(0);
   tdrStyle->SetOptStat(0); // To display the mean and RMS:   SetOptStat("mr");
-  // tdrStyle->SetOptStat("neMR"); // To display without the name:  SetOptstat("eMR");
-  // tdrStyle->SetStatColor(kWhite);
-  tdrStyle->SetStatColor(0);
+  tdrStyle->SetStatColor(kWhite);
   tdrStyle->SetStatFont(42);
   tdrStyle->SetStatFontSize(0.025);
   tdrStyle->SetStatTextColor(1);
@@ -143,25 +174,19 @@ TStyle* getTDRStyle() {
   // tdrStyle->SetStatX(Float_t x = 0);
   // tdrStyle->SetStatY(Float_t y = 0);
 
-// for the legend:
-  tdrStyle->SetLegendBorderSize(0);
-
 // Margins:
   tdrStyle->SetPadTopMargin(0.05);
   tdrStyle->SetPadBottomMargin(0.13);
   tdrStyle->SetPadLeftMargin(0.16);
-  // tdrStyle->SetPadRightMargin(0.02);
-  tdrStyle->SetPadRightMargin(0.08);
+  tdrStyle->SetPadRightMargin(0.02);
 
 // For the Global title:
 
   tdrStyle->SetOptTitle(0);
   tdrStyle->SetTitleFont(42);
-  // tdrStyle->SetTitleColor(1);
-  tdrStyle->SetTitleColor(0,"c");
+  tdrStyle->SetTitleColor(1);
   tdrStyle->SetTitleTextColor(1);
-  // tdrStyle->SetTitleFillColor(10);
-  tdrStyle->SetTitleFillColor(0);
+  tdrStyle->SetTitleFillColor(10);
   tdrStyle->SetTitleFontSize(0.05);
   // tdrStyle->SetTitleH(0); // Set the height of the title box
   // tdrStyle->SetTitleW(0); // Set the width of the title box
@@ -177,10 +202,9 @@ TStyle* getTDRStyle() {
   tdrStyle->SetTitleSize(0.06, "XYZ");
   // tdrStyle->SetTitleXSize(Float_t size = 0.02); // Another way to set the size?
   // tdrStyle->SetTitleYSize(Float_t size = 0.02);
-  tdrStyle->SetTitleXOffset(1.0);
+  tdrStyle->SetTitleXOffset(0.9);
   tdrStyle->SetTitleYOffset(1.25);
-  // tdrStyle->SetTitleOffset(1.3, "X"); // Another way to set the Offset
-  // tdrStyle->SetTitleOffset(1.3, "Y"); // Another way to set the Offset
+  // tdrStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
 
 // For the axis labels:
 
@@ -214,19 +238,305 @@ TStyle* getTDRStyle() {
   // tdrStyle->SetBarWidth(Float_t barwidth = 0.5);
   // tdrStyle->SetPaintTextFormat(const char* format = "g");
   // tdrStyle->SetPalette(Int_t ncolors = 0, Int_t* colors = 0);
-  tdrStyle->SetPalette(1);
   // tdrStyle->SetTimeOffset(Double_t toffset);
   // tdrStyle->SetHistMinimumZero(kTRUE);
 
+  tdrStyle->SetHatchesLineWidth(5);
+  tdrStyle->SetHatchesSpacing(0.05);
+
   tdrStyle->cd();
-  
-  return tdrStyle;
+
 }
 
-// sets the style for the canvas if the plots need to be in tdr style
-void setStyle() {
-   TStyle* tdrStyle = getTDRStyle();
-   gROOT->SetStyle(tdrStyle->GetName());
+////////////////
+// CMS_lumi.h //
+////////////////
+
+//
+// Global variables
+//
+
+TString cmsText     = "CMS";
+float cmsTextFont   = 61;  // default is helvetic-bold
+
+bool writeExtraText = true;//false;
+bool isSimulation = true;//false;
+TString extraText   = "Preliminary";
+float extraTextFont = 52;  // default is helvetica-italics
+
+// text sizes and text offsets with respect to the top frame
+// in unit of the top margin size
+float lumiTextSize     = 0.6;
+float lumiTextOffset   = 0.2;
+float cmsTextSize      = 0.75;
+float cmsTextOffset    = 0.1;  // only used in outOfFrame version
+
+float relPosX    = 0.045;
+float relPosY    = 0.035;
+float relExtraDY = 1.2;
+
+// ratio of "CMS" and extra text size
+float extraOverCmsTextSize  = 0.76;
+
+TString lumi_13TeV = "20.1 fb^{-1}";
+TString lumi_8TeV  = "19.7 fb^{-1}";
+TString lumi_7TeV  = "5.1 fb^{-1}";
+
+bool drawLogo      = false;
+
+void CMS_lumi( TPad* pad, int iPeriod=3, int iPosX=10 );
+
+
+////////////////
+// CMS_lumi.C //
+////////////////
+
+//#include "CMS_lumi.h"
+
+void 
+CMS_lumi( TPad* pad, int iPeriod, int iPosX )
+{            
+  bool outOfFrame    = false;
+  if( iPosX/10==0 ) 
+    {
+      outOfFrame = true;
+    }
+  int alignY_=3;
+  int alignX_=2;
+  if( iPosX/10==0 ) alignX_=1;
+  if( iPosX==0    ) alignX_=1;
+  if( iPosX==0    ) alignY_=1;
+  if( iPosX/10==1 ) alignX_=1;
+  if( iPosX/10==2 ) alignX_=2;
+  if( iPosX/10==3 ) alignX_=3;
+  //if( iPosX == 0  ) relPosX = 0.12;
+  if( iPosX == 0  ) relPosX = pad->GetLeftMargin();
+  int align_ = 10*alignX_ + alignY_;
+
+  float H = pad->GetWh();
+  float W = pad->GetWw();
+  float l = pad->GetLeftMargin();
+  float t = pad->GetTopMargin();
+  float r = pad->GetRightMargin();
+  float b = pad->GetBottomMargin();
+  //  float e = 0.025;
+
+  pad->cd();
+
+  TString lumiText;
+  if( iPeriod==1 )
+    {
+      lumiText += lumi_7TeV;
+      lumiText += " (7 TeV)";
+    }
+  else if ( iPeriod==2 )
+    {
+      lumiText += lumi_8TeV;
+      lumiText += " (8 TeV)";
+    }
+  else if( iPeriod==3 ) 
+    {
+      lumiText = lumi_8TeV; 
+      lumiText += " (8 TeV)";
+      lumiText += " + ";
+      lumiText += lumi_7TeV;
+      lumiText += " (7 TeV)";
+    }
+  else if ( iPeriod==4 )
+    {
+      lumiText += lumi_13TeV;
+      lumiText += " (13 TeV)";
+    }
+  else if ( iPeriod==7 )
+    { 
+      if( outOfFrame ) lumiText += "#scale[0.85]{";
+      lumiText += lumi_13TeV; 
+      lumiText += " (13 TeV)";
+      lumiText += " + ";
+      lumiText += lumi_8TeV; 
+      lumiText += " (8 TeV)";
+      lumiText += " + ";
+      lumiText += lumi_7TeV;
+      lumiText += " (7 TeV)";
+      if( outOfFrame) lumiText += "}";
+    }
+  else if ( iPeriod==12 )
+    {
+      lumiText += "8 TeV";
+    }
+   
+  cout << lumiText << endl;
+
+  TLatex latex;
+  latex.SetNDC();
+  latex.SetTextAngle(0);
+  latex.SetTextColor(kBlack);    
+
+  float extraTextSize = extraOverCmsTextSize*cmsTextSize;
+
+  latex.SetTextFont(42);
+  latex.SetTextAlign(31); 
+  latex.SetTextSize(lumiTextSize*t);    
+  latex.DrawLatex(1-r,1-t+lumiTextOffset*t,lumiText);
+
+  if( outOfFrame )
+    {
+      latex.SetTextFont(cmsTextFont);
+      latex.SetTextAlign(11); 
+      latex.SetTextSize(cmsTextSize*t);    
+      latex.DrawLatex(l,1-t+lumiTextOffset*t,cmsText);
+    }
+  
+  pad->cd();
+
+  float posX_=0;
+  if( iPosX%10<=1 )
+    {
+      posX_ =   l + relPosX*(1-l-r);
+    }
+  else if( iPosX%10==2 )
+    {
+      posX_ =  l + 0.5*(1-l-r);
+    }
+  else if( iPosX%10==3 )
+    {
+      posX_ =  1-r - relPosX*(1-l-r);
+    }
+  float posY_ = 1-t - relPosY*(1-t-b);
+  if( !outOfFrame )
+  {
+     if( drawLogo )
+     {
+        posX_ =   l + 0.045*(1-l-r)*W/H;
+        posY_ = 1-t - 0.045*(1-t-b);
+        float xl_0 = posX_;
+        float yl_0 = posY_ - 0.15;
+        float xl_1 = posX_ + 0.15*H/W;
+        float yl_1 = posY_;
+//    TASImage* CMS_logo = new TASImage("CMS-BW-label.png");
+        TPad* pad_logo = new TPad("logo","logo", xl_0, yl_0, xl_1, yl_1 );
+        pad_logo->Draw();
+        pad_logo->cd();
+//    CMS_logo->Draw("X");
+        pad_logo->Modified();
+        pad->cd();
+     }
+     else
+     {
+        latex.SetTextFont(cmsTextFont);
+        latex.SetTextSize(cmsTextSize*t);
+        latex.SetTextAlign(align_);
+        latex.DrawLatex(posX_, posY_, cmsText);
+        if( writeExtraText ) 
+        {
+           if(isSimulation){
+              latex.SetTextFont(extraTextFont);
+              latex.SetTextAlign(align_);
+              latex.SetTextSize(extraTextSize*t);
+              latex.DrawLatex(posX_, posY_- relExtraDY*cmsTextSize*t, "Simulation");
+              latex.DrawLatex(posX_, posY_- 2*relExtraDY*cmsTextSize*t, extraText);
+           }
+           else{
+              latex.SetTextFont(extraTextFont);
+              latex.SetTextAlign(align_);
+              latex.SetTextSize(extraTextSize*t);
+              latex.DrawLatex(posX_, posY_- relExtraDY*cmsTextSize*t, extraText);
+           }
+        }
+     }
+  }
+  else if( writeExtraText )
+  {
+     if( iPosX==0) 
+     {
+        posX_ =   l +  relPosX*(1-l-r);
+        posY_ =   1-t+lumiTextOffset*t;
+     }
+     latex.SetTextFont(extraTextFont);
+     latex.SetTextSize(extraTextSize*t);
+     latex.SetTextAlign(align_);
+     latex.DrawLatex(posX_, posY_, extraText);      
+  }
+  return;
+}
+
+///////////////
+// myMacro.C //
+///////////////
+
+// Give the macro an empty histogram for h->Draw("AXIS");
+// Create h after calling setTDRStyle to get all the settings right
+TCanvas* tdrCanvas(const char* canvName, TH1D *h,
+       int iPeriod = 2, int iPos = 11,
+       bool square = false) {
+
+  setTDRStyle();
+
+  //writeExtraText = true;       // if extra text
+  //extraText  = "Preliminary";  // default extra text is "Preliminary"
+  //lumi_8TeV  = "19.5 fb^{-1}"; // default is "19.7 fb^{-1}"
+  //lumi_7TeV  = "5.0 fb^{-1}";  // default is "5.1 fb^{-1}"
+  
+  //int iPeriod = 3;    // 1=7TeV, 2=8TeV, 3=7+8TeV, 7=7+8+13TeV 
+
+  // second parameter in example_plot is iPos, which drives the position of the CMS logo in the plot
+  // iPos=11 : top-left, left-aligned
+  // iPos=33 : top-right, right-aligned
+  // iPos=22 : center, centered
+  // iPos=0  : out of frame (in exceptional cases)
+  // mode generally : 
+  //   iPos = 10*(alignement 1/2/3) + position (1/2/3 = left/center/right)
+
+
+  //  if( iPos==0 ) relPosX = 0.12;
+
+  int W = (square ? 600 : 800);
+  int H = (square ? 600 : 600);
+
+  // 
+  // Simple example of macro: plot with CMS name and lumi text
+  //  (this script does not pretend to work in all configurations)
+  // iPeriod = 1*(0/1 7 TeV) + 2*(0/1 8 TeV)  + 4*(0/1 13 TeV) 
+  // For instance: 
+  //               iPeriod = 3 means: 7 TeV + 8 TeV
+  //               iPeriod = 7 means: 7 TeV + 8 TeV + 13 TeV 
+  // Initiated by: Gautier Hamel de Monchenault (Saclay)
+  //
+  int W_ref = (square ? 600 : 800); 
+  int H_ref = (square ? 600 : 600); 
+
+  // references for T, B, L, R
+  float T = (square ? 0.07*H_ref : 0.08*H_ref);
+  float B = (square ? 0.13*H_ref : 0.12*H_ref); 
+  float L = (square ? 0.15*W_ref : 0.12*W_ref);
+  float R = (square ? 0.05*W_ref : 0.04*W_ref);
+
+  TCanvas *canv = new TCanvas(canvName,canvName,50,50,W,H);
+  canv->SetFillColor(0);
+  canv->SetBorderMode(0);
+  canv->SetFrameFillStyle(0);
+  canv->SetFrameBorderMode(0);
+  canv->SetLeftMargin( L/W );
+  canv->SetRightMargin( R/W );
+  canv->SetTopMargin( T/H );
+  canv->SetBottomMargin( B/H );
+  // FOR JEC plots, prefer to keep ticks on both sides
+  //canv->SetTickx(0);
+  //canv->SetTicky(0);
+
+  assert(h);
+  h->GetYaxis()->SetTitleOffset(square ? 1.25 : 1);
+  h->GetXaxis()->SetTitleOffset(square ? 1.0 : 0.9);
+  h->Draw("AXIS");
+
+  // writing the lumi information and the CMS "logo"
+  CMS_lumi( canv, iPeriod, iPos );
+  
+  canv->Update();
+  canv->RedrawAxis();
+  canv->GetFrame()->Draw();
+  
+  return canv;
 }
 
 void cmsPrel(double intLumi=-1, bool wide = false) {
@@ -278,3 +588,5 @@ void cmsFinal(double intLumi=-1, bool wide = false) {
 
 //cmsPrel(); // to print just CMS and \sqrt{s}
 //cmsPrel(400);  // to print also the integrated luminosity.
+
+#endif
