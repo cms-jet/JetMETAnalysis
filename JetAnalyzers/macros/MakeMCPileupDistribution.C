78a8476964392e1d3dcc7795c70651916810a17b
Alexx Perloff <aperloff@physics.tamu.edu>
2016-04-05 17:55:23 -0500

    Update the program to be more user friendly and automated.

--- JetAnalyzers/macros/MakeMCPileupDistribution.C
+++ JetAnalyzers/macros/MakeMCPileupDistribution.C
@@ -1,18 +1,52 @@
-void MakeMCPileupDistribution(TString ifilename = "", TString ofilename = "", TString alg = "") {
-   if(ifilename.IsNull())
-      ifilename = "/fdata/hepx/store/user/aperloff/JRA_outfiles_53X_20140212_pbs/JRA/JRA.root";
-   if(ofilename.IsNull())
-      ofilename = "pileup14_RD_March13_finebin.root";
-      //ofilename = "/fdata/hepx/store/user/aperloff/JEC_532/53X/pileup14_RD_March13_finebin.root";
-   if(alg.IsNull())
-      alg = "ak5pf";
-   TFile* ifile = new TFile(ifilename,"READ");
-   TFile* ofile = new TFile(ofilename,"RECREATE");
-   ifile->cd(alg);
-   TH1D* pileup = new TH1D("pileup","pileup",600,0,60);
-   t->Draw("tnpus[1]>>pileup");
-   pileup->GetXaxis()->SetTitle("tnpus[1]");
-   ofile->cd();
-   pileup->Write();
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TTree.h"
+#include "TH1D.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void MakeMCHistogram(string ipath, string ifilename, string idirectory="",
+                     string itreename="t", string ivar="tnpus") {
+   cout << "MakeMCHistogram::Opening input file ... " << flush;
+   TFile* ifile = TFile::Open((ipath+"/"+ifilename).c_str(),"READ");
+   if(!ifile || !ifile->IsOpen()) {
+      cout << endl << "ERROR MakeMCHistogram::The input file " << ipath << "/" << ifilename
+           << " could not be opened. This program will now exit." << endl;
+      return;
+   }
+   cout << "DONE" << endl;
+
+   cout << "MakeMCHistogram::Retrieving input tree ... " << flush;
+   TTree* itree = (TTree*)ifile->Get((idirectory+"/"+itreename).c_str());
+   if(!itree) {
+      cout << endl << "ERROR MakeMCHistogram::The input TTree could not be retrieved."
+           << " This program will now exit." << endl;
+      return;
+   }
+   cout << "DONE" << endl;
+
+   cout << "MakeMCHistogram::Opening the output file and booking the output histogram ... " << flush;
+   TFile* ofile = TFile::Open("MyMCPileupHistogram.root","RECREATE");
+   TH1D* h = new TH1D("pileup","pileup",60,0,60);
+   cout << "DONE" << endl;
+
+   cout << "MakeMCHistogram::Filling the output histogram ... " << flush;
+   itree->Draw((ivar+">>pileup").c_str(),"","goff");
+   cout << "DONE" << endl;
+
+   cout << "MakeMCHistogram::Writing the output histogram ... " << flush;
+   h->Write();
+   cout << "DONE" << endl;
+
+   cout << "MakeMCHistogram::Closing the files ... " << flush;
    ofile->Close();
+   ifile->Close();
+   cout << "DONE" << endl;
+
+   return;
 }
