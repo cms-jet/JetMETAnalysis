8538ee181fa35a666253417c396be1b11b053449
Alexx Perloff <aperloff@physics.tamu.edu>
2016-04-05 17:54:22 -0500

    Updates to the JRAEvent class and all of the executables that depend upon it. Switched from arrays to vectors and now using a more ntuple like style.

--- JetUtilities/src/JRAEvent.cc
+++ JetUtilities/src/JRAEvent.cc
@@ -7,11 +7,11 @@
 #include "TStyle.h"
 #include "TCanvas.h"
 
-JRAEvent::JRAEvent(TTree *tree, bool newTree) : fChain(0) 
+JRAEvent::JRAEvent(TTree *tree, bitset<8> b) : fChain(0), flags(b)
 {
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
-   if(newTree) {
+   if(!tree || tree->GetNbranches()==0) {
       MakeTree(tree);
    }
    else {
@@ -45,7 +45,7 @@ Long64_t JRAEvent::LoadTree(Long64_t entry)
    return centry;
 }
 
-void JRAEvent::MakeTree(TTree *tree)
+void JRAEvent::MakeTree(TTree* tree)
 {
    gROOT->ProcessLine("#include <vector>");
 
@@ -53,7 +53,7 @@ void JRAEvent::MakeTree(TTree *tree)
    MakeVectors();
 
    // Set branch addresses and branch pointers
-   if (!tree) return;
+   if (!tree) tree = new TTree("t","t");
    fChain = tree;
    fCurrent = -1;
    fChain->SetMakeClass(1);
@@ -68,7 +68,9 @@ void JRAEvent::MakeTree(TTree *tree)
    fChain->Branch("ntrks_highpt", "vector<Int_t>", &ntrks_highpt);
    fChain->Branch("rhos", "vector<Float_t>", &rhos);
    fChain->Branch("rho", &rho, "rho/F");
-   fChain->Branch("rho_hlt", &rho_hlt, "rho_hlt/F");
+   if(!flags.test(0) || (flags.test(0) && flags.test(1))) {
+      fChain->Branch("rho_hlt", &rho_hlt, "rho_hlt/F");
+   }
    fChain->Branch("pthat", &pthat, "pthat/F");
    fChain->Branch("beta", &beta, "beta/F");
    fChain->Branch("betaStar", &betaStar, "betaStar/F");
@@ -79,16 +81,22 @@ void JRAEvent::MakeTree(TTree *tree)
    fChain->Branch("evt", &evt, "evt/L");
    fChain->Branch("nref", &nref, "nref/b");
    fChain->Branch("refrank", "vector<UChar_t>", &refrank);
-   fChain->Branch("refpdgid", "vector<Int_t>", &refpdgid);
-   fChain->Branch("refpdgid_algorithmicDef", "vector<Int_t>", &refpdgid_algorithmicDef);
-   fChain->Branch("refpdgid_physicsDef", "vector<Int_t>", &refpdgid_physicsDef);
+   if(!flags.test(0) || (flags.test(0) && flags.test(2))) {
+      fChain->Branch("refpdgid", "vector<Int_t>", &refpdgid);
+      fChain->Branch("refpdgid_algorithmicDef", "vector<Int_t>", &refpdgid_algorithmicDef);
+      fChain->Branch("refpdgid_physicsDef", "vector<Int_t>", &refpdgid_physicsDef);
+   }
    fChain->Branch("refe", "vector<Float_t>", &refe);
    fChain->Branch("refpt", "vector<Float_t>", &refpt);
    fChain->Branch("refeta", "vector<Float_t>", &refeta);
    fChain->Branch("refphi", "vector<Float_t>", &refphi);
    fChain->Branch("refy", "vector<Float_t>", &refy);
-   fChain->Branch("refdrjt", "vector<Float_t>", &refdrjt);
-   fChain->Branch("refdphijt", "vector<Float_t>", &refdphijt);
+   if(!flags.test(0) || (flags.test(0) && flags.test(3))) {
+      fChain->Branch("refdphijt", "vector<Float_t>", &refdphijt);
+   }
+   if(!flags.test(0) || (flags.test(0) && !flags.test(3))) {
+      fChain->Branch("refdrjt", "vector<Float_t>", &refdrjt);
+   }
    fChain->Branch("refarea", "vector<Float_t>", &refarea);
    fChain->Branch("jte", "vector<Float_t>", &jte);
    fChain->Branch("jtpt", "vector<Float_t>", &jtpt);
@@ -97,22 +105,30 @@ void JRAEvent::MakeTree(TTree *tree)
    fChain->Branch("jty", "vector<Float_t>", &jty);
    fChain->Branch("jtjec", "vector<Float_t>", &jtjec);
    fChain->Branch("jtarea", "vector<Float_t>", &jtarea);
-   fChain->Branch("jtchf", "vector<Float_t>", &jtchf);
-   fChain->Branch("jtnhf", "vector<Float_t>", &jtnhf);
-   fChain->Branch("jtnef", "vector<Float_t>", &jtnef);
-   fChain->Branch("jtcef", "vector<Float_t>", &jtcef);
-   fChain->Branch("jtemf", "vector<Float_t>", &jtemf);
-   fChain->Branch("jtmuf", "vector<Float_t>", &jtmuf);
-   fChain->Branch("jthfhf", "vector<Float_t>", &jthfhf);
-   fChain->Branch("jthfef", "vector<Float_t>", &jthfef);
+   if(!flags.test(0) || (flags.test(0) && flags.test(4))) {
+      if (!flags.test(0) || (flags.test(0) && flags.test(5))) {
+         fChain->Branch("jtemf", "vector<Float_t>", &jtemf);
+      }    
+      if (!flags.test(0) || (flags.test(0) && flags.test(6))) {
+         fChain->Branch("jtchf", "vector<Float_t>", &jtchf);
+         fChain->Branch("jtnhf", "vector<Float_t>", &jtnhf);
+         fChain->Branch("jtnef", "vector<Float_t>", &jtnef);
+         fChain->Branch("jtcef", "vector<Float_t>", &jtcef);
+         fChain->Branch("jtmuf", "vector<Float_t>", &jtmuf);
+         fChain->Branch("jthfhf", "vector<Float_t>", &jthfhf);
+         fChain->Branch("jthfef", "vector<Float_t>", &jthfef);
+      }
+   }
+   if (!flags.test(0) || (flags.test(0) && flags.test(6) && flags.test(7))) {
+      fChain->Branch("pfcand_px", "vector<Float_t>", &pfcand_px);
+      fChain->Branch("pfcand_py", "vector<Float_t>", &pfcand_py);
+      fChain->Branch("pfcand_pt", "vector<Float_t>", &pfcand_pt);
+      fChain->Branch("pfcand_eta", "vector<Float_t>", &pfcand_eta);
+      fChain->Branch("pfcand_phi", "vector<Float_t>", &pfcand_phi);
+      fChain->Branch("pfcand_e", "vector<Float_t>", &pfcand_e);
+      fChain->Branch("pfcand_id", "vector<JRAEvent::Flavor>", &pfcand_id);
+   }
    fChain->Branch("refdzvtx", "vector<Float_t>", &refdzvtx);
-   fChain->Branch("pfcand_px", "vector<Float_t>", &pfcand_px);
-   fChain->Branch("pfcand_py", "vector<Float_t>", &pfcand_py);
-   fChain->Branch("pfcand_pt", "vector<Float_t>", &pfcand_pt);
-   fChain->Branch("pfcand_eta", "vector<Float_t>", &pfcand_eta);
-   fChain->Branch("pfcand_phi", "vector<Float_t>", &pfcand_phi);
-   fChain->Branch("pfcand_e", "vector<Float_t>", &pfcand_e);
-   fChain->Branch("pfcand_id", "vector<Flavor>", &pfcand_id);
    Notify();
 }
 
@@ -135,6 +151,10 @@ void JRAEvent::Init(TTree *tree)
    fCurrent = -1;
    fChain->SetMakeClass(1);
 
+   // Default values in case branch does not exist in tree
+   weight = 1.0;
+
+   // Set branch addresses
    fChain->SetBranchAddress("npus", &npus, &b_npus);
    fChain->SetBranchAddress("tnpus", &tnpus, &b_tnpus);
    fChain->SetBranchAddress("zpositions", &zpositions, &b_zpositions);
@@ -145,7 +165,9 @@ void JRAEvent::Init(TTree *tree)
    fChain->SetBranchAddress("ntrks_highpt", &ntrks_highpt, &b_ntrks_highpt);
    fChain->SetBranchAddress("rhos", &rhos, &b_rhos);
    fChain->SetBranchAddress("rho", &rho, &b_rho);
-   fChain->SetBranchAddress("rho_hlt", &rho_hlt, &b_rho_hlt);
+   if(!flags.test(0) || (flags.test(0) && flags.test(1))) {
+      fChain->SetBranchAddress("rho_hlt", &rho_hlt, &b_rho_hlt);
+   }
    fChain->SetBranchAddress("pthat", &pthat, &b_pthat);
    fChain->SetBranchAddress("beta", &beta, &b_beta);
    fChain->SetBranchAddress("betaStar", &betaStar, &b_betaStar);
@@ -156,16 +178,22 @@ void JRAEvent::Init(TTree *tree)
    fChain->SetBranchAddress("evt", &evt, &b_evt);
    fChain->SetBranchAddress("nref", &nref, &b_nref);
    fChain->SetBranchAddress("refrank", &refrank, &b_refrank);
-   fChain->SetBranchAddress("refpdgid", &refpdgid, &b_refpdgid);
-   fChain->SetBranchAddress("refpdgid_algorithmicDef", &refpdgid_algorithmicDef, &b_refpdgid_algorithmicDef);
-   fChain->SetBranchAddress("refpdgid_physicsDef", &refpdgid_physicsDef, &b_refpdgid_physicsDef);
+   if(!flags.test(0) || (flags.test(0) && flags.test(2))) {
+      fChain->SetBranchAddress("refpdgid", &refpdgid, &b_refpdgid);
+      fChain->SetBranchAddress("refpdgid_algorithmicDef", &refpdgid_algorithmicDef, &b_refpdgid_algorithmicDef);
+      fChain->SetBranchAddress("refpdgid_physicsDef", &refpdgid_physicsDef, &b_refpdgid_physicsDef);
+   }
    fChain->SetBranchAddress("refe", &refe, &b_refe);
    fChain->SetBranchAddress("refpt", &refpt, &b_refpt);
    fChain->SetBranchAddress("refeta", &refeta, &b_refeta);
    fChain->SetBranchAddress("refphi", &refphi, &b_refphi);
    fChain->SetBranchAddress("refy", &refy, &b_refy);
-   fChain->SetBranchAddress("refdrjt", &refdrjt, &b_refdrjt);
-   fChain->SetBranchAddress("refdphijt", &refdphijt, &b_refdphijt);
+   if(!flags.test(0) || (flags.test(0) && flags.test(3))) {
+      fChain->SetBranchAddress("refdphijt", &refdphijt, &b_refdphijt);
+   }
+   if(!flags.test(0) || (flags.test(0) && !flags.test(3))) {
+     fChain->SetBranchAddress("refdrjt", &refdrjt, &b_refdrjt);
+   }
    fChain->SetBranchAddress("refarea", &refarea, &b_refarea);
    fChain->SetBranchAddress("jte", &jte, &b_jte);
    fChain->SetBranchAddress("jtpt", &jtpt, &b_jtpt);
@@ -174,22 +202,30 @@ void JRAEvent::Init(TTree *tree)
    fChain->SetBranchAddress("jty", &jty, &b_jty);
    fChain->SetBranchAddress("jtjec", &jtjec, &b_jtjec);
    fChain->SetBranchAddress("jtarea", &jtarea, &b_jtarea);
-   fChain->SetBranchAddress("jtchf", &jtchf, &b_jtchf);
-   fChain->SetBranchAddress("jtnhf", &jtnhf, &b_jtnhf);
-   fChain->SetBranchAddress("jtnef", &jtnef, &b_jtnef);
-   fChain->SetBranchAddress("jtcef", &jtcef, &b_jtcef);
-   fChain->SetBranchAddress("jtemf", &jtemf, &b_jtemf);
-   fChain->SetBranchAddress("jtmuf", &jtmuf, &b_jtmuf);
-   fChain->SetBranchAddress("jthfhf", &jthfhf, &b_jthfhf);
-   fChain->SetBranchAddress("jthfef", &jthfef, &b_jthfef);
+   if(!flags.test(0) || (flags.test(0) && flags.test(4))) {
+      if (!flags.test(0) || (flags.test(0) && flags.test(5))) {
+         fChain->SetBranchAddress("jtemf", &jtemf, &b_jtemf);
+      }
+      if (!flags.test(0) || (flags.test(0) && flags.test(6))) {
+         fChain->SetBranchAddress("jtchf", &jtchf, &b_jtchf);
+         fChain->SetBranchAddress("jtnhf", &jtnhf, &b_jtnhf);
+         fChain->SetBranchAddress("jtnef", &jtnef, &b_jtnef);
+         fChain->SetBranchAddress("jtcef", &jtcef, &b_jtcef);
+         fChain->SetBranchAddress("jtmuf", &jtmuf, &b_jtmuf);
+         fChain->SetBranchAddress("jthfhf", &jthfhf, &b_jthfhf);
+         fChain->SetBranchAddress("jthfef", &jthfef, &b_jthfef);
+      }
+   }
+   if (!flags.test(0) || (flags.test(0) && flags.test(6) && flags.test(7))) {
+      fChain->SetBranchAddress("pfcand_px", &pfcand_px, &b_pfcand_px);
+      fChain->SetBranchAddress("pfcand_py", &pfcand_py, &b_pfcand_py);
+      fChain->SetBranchAddress("pfcand_pt", &pfcand_pt, &b_pfcand_pt);
+      fChain->SetBranchAddress("pfcand_eta", &pfcand_eta, &b_pfcand_eta);
+      fChain->SetBranchAddress("pfcand_phi", &pfcand_phi, &b_pfcand_phi);
+      fChain->SetBranchAddress("pfcand_e", &pfcand_e, &b_pfcand_e);
+      fChain->SetBranchAddress("pfcand_id", &pfcand_id, &b_pfcand_id);
+   }
    fChain->SetBranchAddress("refdzvtx", &refdzvtx, &b_refdzvtx);
-   fChain->SetBranchAddress("pfcand_px", &pfcand_px, &b_pfcand_px);
-   fChain->SetBranchAddress("pfcand_py", &pfcand_py, &b_pfcand_py);
-   fChain->SetBranchAddress("pfcand_pt", &pfcand_pt, &b_pfcand_pt);
-   fChain->SetBranchAddress("pfcand_eta", &pfcand_eta, &b_pfcand_eta);
-   fChain->SetBranchAddress("pfcand_phi", &pfcand_phi, &b_pfcand_phi);
-   fChain->SetBranchAddress("pfcand_e", &pfcand_e, &b_pfcand_e);
-   fChain->SetBranchAddress("pfcand_id", &pfcand_id, &b_pfcand_id);
    Notify();
 }
 
@@ -232,16 +268,22 @@ void JRAEvent::MakeVectors()
    ntrks_highpt            = new vector<int>;
    rhos                    = new vector<float>;
    refrank                 = new vector<UChar_t>;
-   refpdgid                = new vector<int>;
-   refpdgid_algorithmicDef = new vector<int>;
-   refpdgid_physicsDef     = new vector<int>;
+   if(!flags.test(0) || (flags.test(0) && flags.test(2))) {
+      refpdgid                = new vector<int>;
+      refpdgid_algorithmicDef = new vector<int>;
+      refpdgid_physicsDef     = new vector<int>;
+   }
    refe                    = new vector<float>;
    refpt                   = new vector<float>;
    refeta                  = new vector<float>;
    refphi                  = new vector<float>;
    refy                    = new vector<float>;
-   refdrjt                 = new vector<float>;
-   refdphijt               = new vector<float>;
+   if(!flags.test(0) || (flags.test(0) && flags.test(3))) {
+      refdphijt               = new vector<float>;
+   }
+   if(!flags.test(0) || (flags.test(0) && !flags.test(3))) {
+      refdrjt                 = new vector<float>;
+   }
    refarea                 = new vector<float>;
    jte                     = new vector<float>;
    jtpt                    = new vector<float>;
@@ -250,22 +292,30 @@ void JRAEvent::MakeVectors()
    jty                     = new vector<float>;
    jtjec                   = new vector<float>;
    jtarea                  = new vector<float>;
-   jtchf                   = new vector<float>;
-   jtnhf                   = new vector<float>;
-   jtnef                   = new vector<float>;
-   jtcef                   = new vector<float>;
-   jtemf                   = new vector<float>;
-   jtmuf                   = new vector<float>;
-   jthfhf                  = new vector<float>;
-   jthfef                  = new vector<float>;
+   if(!flags.test(0) || (flags.test(0) && flags.test(4))) {
+      if (!flags.test(0) || (flags.test(0) && flags.test(5))) {
+         jtemf                   = new vector<float>;
+      }
+      if (!flags.test(0) || (flags.test(0) && flags.test(6))) {
+         jtchf                   = new vector<float>;
+         jtnhf                   = new vector<float>;
+         jtnef                   = new vector<float>;
+         jtcef                   = new vector<float>;
+         jtmuf                   = new vector<float>;
+         jthfhf                  = new vector<float>;
+         jthfef                  = new vector<float>;
+      }
+   }
+   if (!flags.test(0) || (flags.test(0) && flags.test(6) && flags.test(7))) {
+      pfcand_px               = new vector<float>;
+      pfcand_py               = new vector<float>;
+      pfcand_pt               = new vector<float>;
+      pfcand_eta              = new vector<float>;
+      pfcand_phi              = new vector<float>;
+      pfcand_e                = new vector<float>;
+      pfcand_id               = new vector<Flavor>;
+   }
    refdzvtx                = new vector<float>;
-   pfcand_px               = new vector<float>;
-   pfcand_py               = new vector<float>;
-   pfcand_pt               = new vector<float>;
-   pfcand_eta              = new vector<float>;
-   pfcand_phi              = new vector<float>;
-   pfcand_e                = new vector<float>;
-   pfcand_id               = new vector<Flavor>;
 }
 
 void JRAEvent::clear()
@@ -280,16 +330,22 @@ void JRAEvent::clear()
    ntrks_highpt->clear();
    rhos->clear();
    refrank->clear();
-   refpdgid->clear();
-   refpdgid_algorithmicDef->clear();
-   refpdgid_physicsDef->clear();
+   if(!flags.test(0) || (flags.test(0) && flags.test(2))) {
+      refpdgid->clear();
+      refpdgid_algorithmicDef->clear();
+      refpdgid_physicsDef->clear();
+   }
    refe->clear();
    refpt->clear();
    refeta->clear();
    refphi->clear();
    refy->clear();
-   refdrjt->clear();
-   refdphijt->clear();
+   if(!flags.test(0) || (flags.test(0) && flags.test(3))) {
+      refdphijt->clear();
+   }
+   if(!flags.test(0) || (flags.test(0) && !flags.test(3))) {
+      refdrjt->clear();
+   }
    refarea->clear();
    jte->clear();
    jtpt->clear();
@@ -298,22 +354,30 @@ void JRAEvent::clear()
    jty->clear();
    jtjec->clear();
    jtarea->clear();
-   jtchf->clear();
-   jtnhf->clear();
-   jtnef->clear();
-   jtcef->clear();
-   jtemf->clear();
-   jtmuf->clear();
-   jthfhf->clear();
-   jthfef->clear();
+   if(!flags.test(0) || (flags.test(0) && flags.test(4))) {
+      if (!flags.test(0) || (flags.test(0) && flags.test(5))) {
+         jtemf->clear();
+      }
+      if (!flags.test(0) || (flags.test(0) && flags.test(6))) {
+         jtchf->clear();
+         jtnhf->clear();
+         jtnef->clear();
+         jtcef->clear();
+         jtmuf->clear();
+         jthfhf->clear();
+         jthfef->clear();
+      }
+   }
+   if (!flags.test(0) || (flags.test(0) && flags.test(6) && flags.test(7))) {
+      pfcand_px->clear();
+      pfcand_py->clear();
+      pfcand_pt->clear();
+      pfcand_eta->clear();
+      pfcand_phi->clear();
+      pfcand_e->clear();
+      pfcand_id->clear();
+   }
    refdzvtx->clear();
-   pfcand_px->clear();
-   pfcand_py->clear();
-   pfcand_pt->clear();
-   pfcand_eta->clear();
-   pfcand_phi->clear();
-   pfcand_e->clear();
-   pfcand_id->clear();
 }
 
 void JRAEvent::Loop()
