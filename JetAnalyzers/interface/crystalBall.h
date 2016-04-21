61801b4820d38d3f695f3ffd2b59cfb76bb2db31
betty calpas <betty.calpas@cern.ch>
2016-04-21 15:18:39 +0300

    mv files to JetUtilities pkg.

--- JetAnalyzers/interface/crystalBall.h
+++
@@ -1,7 +0,0 @@
-#ifndef CRYSTALBALL_HH
-#define CRYSTALBALL_HH
-
-double crystalBall(double *xx, double *pp); // global fit using the function above
-double crystalBall_1(double *xx, double *pp); // global fit using the function above
-
-#endif
