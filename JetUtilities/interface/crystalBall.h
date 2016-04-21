0f73e5f4798a6aa76f70304590ae8f2aafd9668d
betty calpas <betty.calpas@cern.ch>
2016-04-21 15:20:57 +0300

    Files for CrystalBall function.

---
+++ JetUtilities/interface/crystalBall.h
@@ -0,0 +1,7 @@
+#ifndef CRYSTALBALL_HH
+#define CRYSTALBALL_HH
+
+double crystalBall(double *xx, double *pp); // global fit using the function above
+double crystalBall_1(double *xx, double *pp); // global fit using the function above
+
+#endif
