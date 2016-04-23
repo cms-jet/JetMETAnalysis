6c09ed51401443d614ab03918245241673c2ddb0
betty calpas <betty.calpas@cern.ch>
2016-04-23 17:43:18 +0300

    Update Alexx comments.

--- JetUtilities/src/crystalBall.cc
+++ JetUtilities/src/crystalBall.cc
@@ -2,16 +2,6 @@
 
 #include "TMath.h"
 
-double sum_gaus_fnc(double *x, double *par); // sum of 3gaus+pol7
-
-double sum_gaus_pol_fnc(double *x, double *par); // sum of 3gaus+pol7
-
-double exp_fcn(double x, double norm, double slope); // exp 
-
-double gauss_fcn(double x, double norm, double mean, double sigma); // gauss 
-
-double pol_fcn(double x, double p0, double p1=0, double p2=0, double p3=0, double p4=0, double p5=0, double p6=0, double p7=0); // pol7
-
 
 // global fit
 double crystalBall(double *xx, double *pp)
