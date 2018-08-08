eee72007c3ba6d8ef75a9c14dfea0af7964db0e1
Sifu Luo <sifuluo@tamu.edu>
2018-08-08 03:47:28 -0500

    compareJEC now works with commandline configurations and can generate dummyl3 files upon request.

--- JetUtilities/interface/L2Creator.hh
+++ JetUtilities/interface/L2Creator.hh
@@ -68,7 +68,7 @@ public:
     L2Creator();
     L2Creator(CommandLine& cl);
 
-    bool     checkFormulaEvaluator();
+    bool     checkFormulaEvaluator(float ptclip);
     void     closeFiles();
     /// check if a vector of strings contains a certain element
     bool     contains(const vector<string>& collection,const string& element);
@@ -92,7 +92,7 @@ public:
     void     setAndFitFLogAndFGaus(TGraphErrors* gabscor, TF1* flog, TF1* fgaus, double xmin);
     void     setOfflinePFParameters(TGraphErrors* gabscor, TF1* fabscor, double xmin, double xmax);
     void     writeTextFileForCurrentAlgorithm();
-    void     writeTextFileForCurrentAlgorithm_spline();
+    void     writeTextFileForCurrentAlgorithm_spline(float ptclip);
 
 private:
     string                    input, era, l3input, histMet;
@@ -118,7 +118,6 @@ private:
     vector<PiecewiseSpline*>  vabscor_eta_spline;
     vector<int>               ptclipcones;
     vector<float>             ptclips;
-    float                     ptclip;
 };
 
 #endif
