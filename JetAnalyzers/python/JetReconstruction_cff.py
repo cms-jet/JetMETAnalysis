5588ac55f5a32e47ba3424ec1477d021fa188473
izisopou <54803536+izisopou@users.noreply.github.com>
2020-10-28 18:15:42 +0200

    Update JetReconstruction_cff.py

--- JetAnalyzers/python/JetReconstruction_cff.py
+++ JetAnalyzers/python/JetReconstruction_cff.py
@@ -87,7 +87,9 @@ ak10PFJets      = ak4PFJets.clone   ( rParam=1.0 )
 #!
 #! PF JETS CHS
 #!
-ak1PFCHSJets    = ak1PFJets.clone( src = 'pfNoPileUpJME' )
+
+
+ak1PFCHSJets    = ak1PFJets.clone( src = 'pfCHS' )
 ak2PFCHSJets    = ak1PFCHSJets.clone( rParam=0.2 )
 ak3PFCHSJets    = ak1PFCHSJets.clone( rParam=0.3 )
 ak4PFCHSJets    = ak1PFCHSJets.clone( rParam=0.4 )
