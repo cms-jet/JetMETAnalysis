f024f83c3f8d5a2f2d5da7513ba0629e29cfea07
izisopou <54803536+izisopou@users.noreply.github.com>
2020-11-03 18:57:38 +0200

    Update jet_correction_analyzer_x.cc
    This change includes the XrdClFileSystem_v2.hh script instead of the XrdClFileSystem.hh so as to avoid compilation errors.

--- JetAnalyzers/bin/jet_correction_analyzer_x.cc
+++ JetAnalyzers/bin/jet_correction_analyzer_x.cc
@@ -19,7 +19,7 @@
 #include "PhysicsTools/Utilities/interface/LumiReWeighting.h"
 #if __has_include("xrootd/XrdCl/XrdClFileSystem.hh")
 //#include "xrootd/XrdCl/XrdClFileSystem.hh"
-#include "condor/XrdClFileSystem_v2.hh"
+#include "JetMETAnalysis/JetAnalyzers/XrdClFileSystem_v2.hh"
 #define has_xrdcl 1
 #else
 #define has_xrdcl 0
