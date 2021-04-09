02a4c2c6c44e409e4e291546dbf83b5eef4e8307
Marino Missiroli <marino.missiroli@cern.ch>
2021-04-09 21:13:04 +0200

    updated JetAnalyzers/interface/Settings.h

--- JetAnalyzers/interface/Settings.h
+++ JetAnalyzers/interface/Settings.h
@@ -16,13 +16,13 @@
 const int NETA                        = 82;
 const int NETA_Half                   = 41;
 const int NETA_Coarse                 = 8;
-const int NETA_HLT                    = 78;
+const int NETA_HLT                    = 34;
 //const int NPtBins                     = 37;
 const int NPtBins                     = 51;
 const int NPtBinsHLT                  = 40;
 const int NPhi                        = 11;
 const int NPileup                     = 12;
-const int NAlgs                       = 151;
+const int NAlgs                       = 159;
 const int NRespBins                   = 600;
 const int RespLow                     = 0;
 const int RespHigh                    = 6;
@@ -35,7 +35,7 @@ const int RhoHigh                     = 100;
 const int NTNPU                       = 200;
 const int NNPU                        = 200;
 const int NAlgorithms                 = 6;
-const int NJetTypes                   = 27;
+const int NJetTypes                   = 29;
 const int NCorrectionLevels           = 7;
 const int NConeSizes                  = 10;
 const int NDetectorNames              = 4;
@@ -63,14 +63,43 @@ const char eta_boundaries[NETA+1][10] = {"-5.191","-4.889","-4.716","-4.538","-4
 
 const char eta_boundaries_coarse[NETA_Coarse+1][10] = {"-5","-3","-2.5","-1.3","0","1.3","2.5","3","5"};
 
-const char eta_boundaries_HLT[NETA_HLT+1][10] = {"-5.191","-4.538","-4.363","-4.191","-4.013","-3.839","-3.664","-3.489",
-"-3.314", "-3.139","-2.964","-2.853","-2.65", "-2.5",  "-2.322","-2.172","-2.043","-1.93",
-"-1.83",  "-1.74", "-1.653","-1.566","-1.479","-1.392","-1.305","-1.218","-1.131","-1.044",
-"-0.957", "-0.879","-0.783","-0.696","-0.609","-0.522","-0.435","-0.348","-0.261","-0.174",
-"-0.087", "0",     "0.087", "0.174", "0.261", "0.348", "0.435" ,"0.522", "0.609", "0.696",
-"0.783",  "0.879", "0.957", "1.044", "1.131", "1.218", "1.305" ,"1.392", "1.479", "1.566",
-"1.653",  "1.74",  "1.83",  "1.93",  "2.043", "2.172", "2.322" ,"2.5",   "2.65",  "2.853",
-"2.964",  "3.139", "3.314", "3.489", "3.664", "3.839", "4.013" ,"4.191", "4.363", "4.538","5.191"};
+const char eta_boundaries_HLT[NETA_HLT+1][10] = {
+ "-5.1",
+ "-4.8",
+ "-4.5",
+ "-4.2",
+ "-3.9",
+ "-3.6",
+ "-3.3",
+ "-3.0",
+ "-2.7",
+ "-2.4",
+ "-2.1",
+ "-1.8",
+ "-1.5",
+ "-1.2",
+ "-0.9",
+ "-0.6",
+ "-0.3",
+  "0.0",
+  "0.3",
+  "0.6",
+  "0.9",
+  "1.2",
+  "1.5",
+  "1.8",
+  "2.1",
+  "2.4",
+  "2.7",
+  "3.0",
+  "3.3",
+  "3.6",
+  "3.9",
+  "4.2",
+  "4.5",
+  "4.8",
+  "5.1",
+};
 
 const char pileup_boundaries[NPileup][10] = {"0","0","1","5","6","10","11","15","16","25","26","inf"};
 
@@ -90,16 +119,45 @@ const double veta_half[NETA_Half+1] = {0, 0.087, 0.174, 0.261, 0.348, 0.435 ,0.5
 2.964,  3.139, 3.314, 3.489, 3.664, 3.839, 4.013 ,4.191, 4.363, 4.538,
 4.716,4.889,5.191};
 
-const double veta_coarse[NETA_Coarse+1] = {-5.0,-3.0,-2.5,-1.3,0,1.3,2.5,3,5};
+const double veta_coarse[NETA_Coarse+1] = {-5.0, -3.0, -2.5, -1.3, 0, 1.3, 2.5, 3.0, 5.0};
 
-const double veta_HLT[NETA_HLT+1] = {-5.191,-4.538,-4.363,-4.191,-4.013,-3.839,-3.664,-3.489,
--3.314, -3.139,-2.964,-2.853,-2.65, -2.5,  -2.322,-2.172,-2.043,-1.93,
--1.83,  -1.74, -1.653,-1.566,-1.479,-1.392,-1.305,-1.218,-1.131,-1.044,
--0.957, -0.879,-0.783,-0.696,-0.609,-0.522,-0.435,-0.348,-0.261,-0.174,
--0.087, 0,     0.087, 0.174, 0.261, 0.348, 0.435 ,0.522, 0.609, 0.696,
-0.783,  0.879, 0.957, 1.044, 1.131, 1.218, 1.305 ,1.392, 1.479, 1.566,
-1.653,  1.74,  1.83,  1.93,  2.043, 2.172, 2.322 ,2.5,   2.65,  2.853,
-2.964,  3.139, 3.314, 3.489, 3.664, 3.839, 4.013 ,4.191, 4.363, 4.538,5.191};
+const double veta_HLT[NETA_HLT+1] = {
+ -5.1,
+ -4.8,
+ -4.5,
+ -4.2,
+ -3.9,
+ -3.6,
+ -3.3,
+ -3.0,
+ -2.7,
+ -2.4,
+ -2.1,
+ -1.8,
+ -1.5,
+ -1.2,
+ -0.9,
+ -0.6,
+ -0.3,
+  0.0,
+  0.3,
+  0.6,
+  0.9,
+  1.2,
+  1.5,
+  1.8,
+  2.1,
+  2.4,
+  2.7,
+  3.0,
+  3.3,
+  3.6,
+  3.9,
+  4.2,
+  4.5,
+  4.8,
+  5.1,
+};
 
 //const double vpt[NPtBins+1] = {10,10.5,11,11.5,12,12.5,13,13.5,14,15,17,20,23,27,30,35,40,45,57,72,90,120,150,200,300,400,550,750,1000,1500,2000,2500,3000,3500,4000,4500,5000,10000};
 const double vpt[NPtBins+1] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,20,23,27,30,35,40,45,57,72,90,120,150,
@@ -132,15 +190,15 @@ const double vnpu[NNPU+1] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,2
 
 const double vpileup[NPileup] = {0,0,1,5,6,10,11,15,16,25,26,1000};
 
-const char algs_all[NAlgs][20] = {"ic5calo","ak4calo","ak5calo","gk5calo","sc5calo","kt4calo","kt5calo","ca4calo","ca5calo","ak5calol1","ak5calol1off","ak5calol1offl2l3","ic5calol2l3","ak5calol2l3","gk5calol2l3","sc5calol2l3","kt4calol2l3","kt5calol2l3","ca4calol2l3","ca5calol2l3","ak7calo","gk7calo","sc7calo","kt6calo","kt7calo","ca6calo","ca7calo","ak7calol1","ak7calol1off","ak7calol2l3","gk7calol2l3","sc7calol2l3","kt6calol2l3","kt7calol2l3","ca6calol2l3","ca7calol2l3","ak5caloHLT","ak5caloHLTl1","ic5pf","ak5pf","gk5pf","sc5pf","kt4pf","kt5pf","ca4pf","ca5pf","ak5pfl1","ak5pfl1l2l3","ak5pfl1off","ic5pfl2l3","ak5pfl2l3","gk5pfl2l3","sc5pfl2l3","kt4pfl2l3","kt5pfl2l3","ca4pfl2l3","ca5pfl2l3","ak7pf","gk7pf","sc7pf","kt6pf","kt7pf","ca6pf","ca7pf","ak7pfl1","ak7pfl1off","ak7pfl2l3","gk7pfl2l3","sc7pfl2l3","kt6pfl2l3","kt7pfl2l3","ca6pfl2l3","ca7pfl2l3","ak5pfchs","ak5pfchsl1","ak5pfchsl1l2l3","ak5pfchsl1off","ak7pfchs","ak7pfchsl1","ak7pfchsl1off","ak5pfHLT","ak5pfHLTl1","ak5pfchsHLT","ak5pfchsHLTl1","ic5jpt","sc5jpt","ak5jpt","ak7jpt","ak5jptl1","ak5jptl1off","ak5jptl1l2l3","ak7jptl1","ak7jptl1off","ak1pf","ak2pf","ak3pf","ak4pf","ak6pf","ak8pf","ak9pf","ak10pf","ak1pfl1","ak2pfl1","ak3pfl1","ak4pfl1","ak6pfl1","ak8pfl1","ak9pfl1","ak10pfl1","ak1pfchs","ak2pfchs","ak3pfchs","ak4pfchs","ak6pfchs","ak8pfchs","ak9pfchs","ak10pfchs","ak1pfchsl1","ak2pfchsl1","ak3pfchsl1","ak4pfchsl1","ak6pfchsl1","ak8pfchsl1","ak9pfchsl1","ak10pfchsl1","ak5calordl1","ak5pfrdl1","ak5pfchsrdl1","ak7calordl1","ak7pfrdl1","ak7pfchsrdl1","ak1puppi","ak2puppi","ak3puppi","ak4puppi","ak5puppi","ak6puppi","ak7puppi","ak8puppi","ak9puppi","ak10puppi","ak1puppil1","ak2puppil1","ak3puppil1","ak4puppil1","ak5puppil1","ak6puppil1","ak7puppil1","ak8puppil1","ak9puppil1","ak10puppil1"};
+const char algs_all[NAlgs][20] = {"ic5calo","ak4calo","ak5calo","gk5calo","sc5calo","kt4calo","kt5calo","ca4calo","ca5calo","ak5calol1","ak5calol1off","ak5calol1offl2l3","ic5calol2l3","ak5calol2l3","gk5calol2l3","sc5calol2l3","kt4calol2l3","kt5calol2l3","ca4calol2l3","ca5calol2l3","ak7calo","gk7calo","sc7calo","kt6calo","kt7calo","ca6calo","ca7calo","ak7calol1","ak7calol1off","ak7calol2l3","gk7calol2l3","sc7calol2l3","kt6calol2l3","kt7calol2l3","ca6calol2l3","ca7calol2l3","ak5caloHLT","ak5caloHLTl1","ic5pf","ak5pf","gk5pf","sc5pf","kt4pf","kt5pf","ca4pf","ca5pf","ak5pfl1","ak5pfl1l2l3","ak5pfl1off","ic5pfl2l3","ak5pfl2l3","gk5pfl2l3","sc5pfl2l3","kt4pfl2l3","kt5pfl2l3","ca4pfl2l3","ca5pfl2l3","ak7pf","gk7pf","sc7pf","kt6pf","kt7pf","ca6pf","ca7pf","ak7pfl1","ak7pfl1off","ak7pfl2l3","gk7pfl2l3","sc7pfl2l3","kt6pfl2l3","kt7pfl2l3","ca6pfl2l3","ca7pfl2l3","ak5pfchs","ak5pfchsl1","ak5pfchsl1l2l3","ak5pfchsl1off","ak7pfchs","ak7pfchsl1","ak7pfchsl1off","ak5pfHLT","ak5pfHLTl1","ak5pfchsHLT","ak5pfchsHLTl1","ic5jpt","sc5jpt","ak5jpt","ak7jpt","ak5jptl1","ak5jptl1off","ak5jptl1l2l3","ak7jptl1","ak7jptl1off","ak1pf","ak2pf","ak3pf","ak4pf","ak6pf","ak8pf","ak9pf","ak10pf","ak1pfl1","ak2pfl1","ak3pfl1","ak4pfl1","ak6pfl1","ak8pfl1","ak9pfl1","ak10pfl1","ak1pfchs","ak2pfchs","ak3pfchs","ak4pfchs","ak6pfchs","ak8pfchs","ak9pfchs","ak10pfchs","ak1pfchsl1","ak2pfchsl1","ak3pfchsl1","ak4pfchsl1","ak6pfchsl1","ak8pfchsl1","ak9pfchsl1","ak10pfchsl1","ak5calordl1","ak5pfrdl1","ak5pfchsrdl1","ak7calordl1","ak7pfrdl1","ak7pfchsrdl1","ak1puppi","ak2puppi","ak3puppi","ak4puppi","ak5puppi","ak6puppi","ak7puppi","ak8puppi","ak9puppi","ak10puppi","ak1puppil1","ak2puppil1","ak3puppil1","ak4puppil1","ak5puppil1","ak6puppil1","ak7puppil1","ak8puppil1","ak9puppil1","ak10puppil1","ak4pfpuppiHLT","ak4pfpuppiHLTl1","ak8pfpuppiHLT","ak8pfpuppiHLTl1","ak4pfclusterHLT","ak4pfclusterHLTl1","ak8pfclusterHLT","ak8pfclusterHLTl1"};
 
 const double drmax_all[NAlgs] = {0.210,0.250,0.210,0.240,0.200,0.240,0.190,0.220,0.250,0.250,0.250,0.210,0.205,0.210,0.240,0.200,0.240,0.190,0.220,0.350,0.290,0.340,0.275,0.325,0.250,0.300,0.350,0.350,0.280,0.290,0.340,0.275,0.325,0.250,0.300,0.250,0.250,0.105,0.250,0.110,0.135,0.120,0.145,0.115,0.125,0.250,0.250,0.250,0.105,0.110,0.110,0.135,0.120,0.145,0.115,0.125,0.350,0.150,0.180,0.180,0.210,0.150,0.190,0.350,0.350,0.150,0.150,0.180,0.180,0.210,0.150,0.190,0.250,0.250,0.250,0.250,0.350,0.350,0.350,0.250,0.250,0.250,0.250,0.210,0.240,0.205,0.205,0.205,0.205,0.205,0.205,0.205,0.150,0.200,0.300,0.400,0.450,0.500,0.150,0.200,0.300,0.400,0.450,0.500,0.150,0.200,0.300,0.400,0.450,0.500,0.150,0.200,0.300,0.400,0.450,0.500,0.250,0.250,0.250,0.350,0.350,0.350};
 
 const TString algorithms[NAlgorithms] = {"IC","GK","SC","KT","AK","CA"};
 
-const TString jet_types[NJetTypes] = {"Calo","JPT","TRK","PF","PFchs","PFPuppi","PUPPI",
-		"CaloHLT","PFHLT","PFchsHLT","JPTHLT",
-		"tau",
+const TString jet_types[NJetTypes] = {"Calo", "JPT", "TRK", "PF", "PFchs", "PFPuppi", "PUPPI",
+               "CaloHLT", "PFHLT", "PFchsHLT", "JPTHLT", "PFPuppiHLT", "PFClusterHLT",
+               "tau",
         "tauHPSlooseCombDBcorrAll",
         "tauHPSlooseCombDBcorrOneProng0Pi0",
         "tauHPSlooseCombDBcorrOneProng1Pi0",
